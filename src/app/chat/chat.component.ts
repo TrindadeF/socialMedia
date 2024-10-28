e6494e1a29ef6464f30bf0368089feda087bb472
Luiz Felipe <luiztrindade01@outlook.com>
2024-10-28 14:33:40 -0300

    chat settings

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ApiService } from '../api.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Message, User, Chat } from 'database';
 import { io, Socket } from 'socket.io-client';
 
@@ -15,10 +15,15 @@ export class ChatComponent implements OnInit, OnDestroy {
   currentUser!: User;
   newMessage: string = '';
   socket: Socket;
-  mutualLike: any;
   messages: Message[] = [];
+  senderId: string = '';
+  receiverId: string = '';
 
-  constructor(private apiService: ApiService, private route: ActivatedRoute) {
+  constructor(
+    private apiService: ApiService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
     this.socket = io('http://localhost:3000');
   }
 
@@ -27,94 +32,117 @@ export class ChatComponent implements OnInit, OnDestroy {
     if (userId) {
       this.apiService.getUserById(userId).subscribe((user: User) => {
         this.currentUser = user;
+        this.senderId = user._id;
         this.loadChats();
       });
     }
 
     this.socket.on('receiveMessage', (message: Message) => {
-      const chat = this.chats.find((c) =>
-        c.participants.some((p: User) => p._id === message.sender)
-      );
-      if (chat) {
-        chat.messages.push(message);
-        chat.lastMessage = message;
-        if (
-          this.selectedChat?.participants.some((p) => p._id === message.sender)
-        ) {
-          this.messages.push(message);
-        }
-      }
+      this.handleIncomingMessage(message);
     });
   }
 
-  loadChats() {
-    const userId = this.currentUser._id;
-    this.apiService.getChats(userId).subscribe(
-      (chats: Chat[]) => {
+  loadChats(): void {
+    this.apiService
+      .getChats(this.senderId, this.senderId)
+      .subscribe((chats: Chat[]) => {
         this.chats = chats;
-      },
-      (error: any) => {
-        console.error('Erro ao carregar chats:', error);
-      }
-    );
+      });
   }
 
   loadMessages(receiverId: string): void {
-    const senderId = this.currentUser._id;
-
-    this.apiService.getMessagesBetweenUsers(senderId, receiverId).subscribe(
-      (messages: Message[]) => {
+    this.apiService
+      .getMessagesBetweenUsers(this.senderId, receiverId)
+      .subscribe((messages) => {
         this.messages = messages;
-      },
-      (error) => {
-        console.error('Erro ao carregar mensagens:', error);
-      }
-    );
+      });
   }
 
   selectChat(chat: Chat): void {
+    console.log('Chat selecionado:', chat);
     this.selectedChat = chat;
+
     const receiverId =
       chat.participants.find((p) => p._id !== this.currentUser._id)?._id || '';
-    this.loadMessages(receiverId);
-    console.log('Chat selecionado:', this.selectedChat);
+
+    if (receiverId) {
+      console.log('Receiver ID encontrado:', receiverId);
+      this.loadMessages(receiverId);
+    } else {
+      console.warn('Receiver não encontrado');
+    }
   }
 
   sendMessage(): void {
-    if (this.newMessage.trim() && this.selectedChat) {
-      const receiverId =
-        this.selectedChat.participants.find(
-          (p) => p._id !== this.currentUser._id
-        )?._id || '';
+    if (!this.selectedChat) {
+      console.warn('Chat não selecionado');
+      return;
+    }
 
-      if (receiverId) {
-        this.apiService
-          .sendMessage(this.currentUser._id, receiverId, this.newMessage)
-          .subscribe({
-            next: (response) => {
-              console.log('Mensagem enviada com sucesso:', response);
-              this.newMessage = '';
-              const messageData: Message = {
-                sender: this.currentUser._id,
-                receiver: receiverId,
-                content: this.newMessage,
-                timestamp: new Date(),
-              };
-              this.socket.emit('sendMessage', messageData);
-            },
-            error: (error) => {
-              console.error('Erro ao enviar mensagem:', error);
-            },
-          });
-      } else {
-        console.warn('Receiver não encontrado');
+    if (this.newMessage.trim() === '') {
+      console.warn('Mensagem vazia');
+      return;
+    }
+
+    const receiverId =
+      this.selectedChat.participants.find((p) => p._id !== this.currentUser._id)
+        ?._id || '';
+
+    if (receiverId) {
+      const messageData: Message = {
+        sender: this.currentUser._id,
+        receiver: receiverId,
+        content: this.newMessage,
+        timestamp: new Date(),
+      };
+
+      this.apiService
+        .sendMessage(this.currentUser._id, receiverId, this.newMessage)
+        .subscribe({
+          next: (response: Message) => {
+            console.log('Mensagem enviada com sucesso:', response);
+            this.newMessage = '';
+            this.messages.push(response);
+            this.socket.emit('sendMessage', response);
+          },
+          error: (error) => {
+            console.error('Erro ao enviar mensagem:', error);
+          },
+        });
+    } else {
+      console.warn('Receiver não encontrado');
+    }
+  }
+
+  private handleIncomingMessage(message: Message) {
+    const chat = this.chats.find((c) =>
+      c.participants.some(
+        (p) => p._id === message.sender || p._id === message.receiver
+      )
+    );
+    if (chat) {
+      chat.messages.push(message);
+      chat.lastMessage = message;
+      if (
+        this.selectedChat?.participants.some(
+          (p) => p._id === message.sender || p._id === message.receiver
+        )
+      ) {
+        this.messages.push(message);
       }
     } else {
-      console.warn('Mensagem vazia ou chat não selecionado');
+      this.loadChats();
     }
   }
 
   ngOnDestroy(): void {
     this.socket.disconnect();
+  }
+
+  getParticipantName(chat: Chat): string | undefined {
+    const participant = chat.participants.find(
+      (p) => p._id !== this.currentUser._id
+    );
+    return participant ? participant.name : undefined;
   }
 }
