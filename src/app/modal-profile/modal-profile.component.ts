dfdda716c98528ad23fe9aa6542d209774e4cd49
Andre Ramalho <ramalhompi@gmail.com>
2024-11-05 15:46:58 -0300

    comments design

--- src/app/modal-profile/modal-profile.component.ts
+++ src/app/modal-profile/modal-profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ApiService } from '../api.service';
 import { Post, User } from 'database';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-modal-profile',
@@ -19,23 +20,42 @@ export class ModalProfileComponent {
   currentUser!: User;
   mutualLikes: { [key: string]: boolean } = {};
   @Input() postContent: string = '';
-  @Input() feedType: 'primaryFeed' | 'secondFeed' = 'primaryFeed'
+  @Input() postId: string = '';
+  @Input() feedType: 'primaryFeed' | 'secondFeed' = 'secondFeed'
+  
 
   comments: string[] = []; 
   newComment: string = ''; 
-  apiService: any;
   @Output() publish = new EventEmitter<{ content: string | null; media: File[]; feedType: 'primaryFeed' | 'secondFeed' }>();
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.getPosts();
+    this.userId = this.getUserIdFromAuthService();
+  }
+  getUserIdFromAuthService(): string {
+    throw new Error('Method not implemented.');
+  }
+  
+  getPosts() {
+    throw new Error('Method not implemented.');
+  }
+
 
   open() {
     this.show = true;
   }
 
+
   close() {
     this.show = false;
     this.closeEvent.emit();
   }
+  
 
-  likePost(postId: string) {
+
+  likePost(postId: string)  {
     this.apiService.likePostInSecondFeed(postId).subscribe({
       next: () => console.log(`Post ${postId} curtido com sucesso!`),
       error: (error:any) => console.error(`Erro ao curtir o post:`, error),
@@ -43,12 +63,38 @@ export class ModalProfileComponent {
   }
 
   
-  addComment() {
-    if (this.newComment.trim() !== '') {
-      this.comments.push(this.newComment);
-      this.newComment = '';
+  loadComments(): void {
+    this.apiService.getPostWithComments(this.postId).subscribe(
+      (data: { comments: any[] }) => {
+        this.comments = data.comments;
+      },
+      (error: any) => {
+        console.error('Erro ao carregar comentários:', error);
+      }
+    );
+  }
+
+
+
+  addComment(): void {
+    if (this.newComment.trim()) {
+      this.apiService
+        .addCommentInSecondFeed(this.postId, this.newComment)
+        .subscribe(
+          () => {
+            console.log('Comentário adicionado com sucesso!');
+            this.loadComments();
+            this.newComment = '';
+          },
+          (error: any) => {
+            console.error('Erro ao adicionar comentário:', error);
+          }
+        );
+    } else {
+      console.error('O comentário não pode estar vazio.');
     }
   }
+
 
  
 
